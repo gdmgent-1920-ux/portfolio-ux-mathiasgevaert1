2087c9a49629a18c778303b5ba0e51885defef91
mathiasgevaert1 <61147841+mathiasgevaert1@users.noreply.github.com>
2020-02-27 19:06:32 +0100

    changes

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/baebde59-59fa-4059-61eb-871d49c49231-ccda/",
                 "target": "_blank"
             }
         ]
@@ -92,8 +92,7 @@ const data = {
                 "target": "_blank"
             }
         ]
-    },
-
+    },   
     /**
      * Assignment 4
      */
