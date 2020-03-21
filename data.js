a02689ae30ab57ce619a2e6700ce9ecac13bd9c3
mathiasgevaert1 <61147841+mathiasgevaert1@users.noreply.github.com>
2020-03-21 15:15:50 +0100

    opdracht 2 afgewerkt - mathias gevaert - AVDa

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/4f8dc569-da66-4fd1-7778-e48b097b52d9-39bc/",
                 "target": "_blank"
             }
         ]
